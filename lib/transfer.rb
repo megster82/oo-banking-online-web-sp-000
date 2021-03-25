4420be4e509ba779c75e681c5c3835a51effc661
meaghan jennings <meaghanljennings@gmail.com>
2021-03-25 23:49:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,12 @@
 class Transfer
+  attr_accessor :sender, :receiver, :amount 
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
   
   
 end
