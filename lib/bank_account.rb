85f6031aeeab8c661498a18050021026784990a3
meaghan jennings <meaghanljennings@gmail.com>
2021-03-26 00:03:37 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -18,7 +18,7 @@ class BankAccount
     "Your balance is $#{balance}."
   end
   
-  def valid?(BankAccount)
+  def valid?
     if BankAccount.status == "open" && BankAccount.balance > 0 
       true
     else
